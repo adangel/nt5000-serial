a3f269ac3f91c3cfccc31380c5510bdc51943a0e
Andreas Dangel <andreas.dangel@adangel.org>
2022-04-10 15:38:56 +0200

    Add datetime subcommand

--- README.md
+++ README.md
@@ -30,3 +30,30 @@ Receive: 13 bytes in buffer
 7. PAC (Power AC): ($uac*$iac)/1000
 8. Energy Today: (buffer[6] * 256 + buffer[7])/1000
 9. Energy Total: buffer[8] * 256 + buffer[9]; 
+
+## Read time
+
+Send: "\x00\x01\x06\x01\x08". Last byte is checksum, 5 bytes in total
+Receive: 13 bytes
+
+1. year
+2. month
+3. day
+4. hour
+5. minute
+
+Remaining 7 bytes are zero, 13th (last) byte is checksum.
+
+## Set time
+
+Multiple commands:
+1. Set year: "\x00\x01\x50"
+2. Set month: "\x00\x01\x51"
+3. Set day: "\x00\x01\x52"
+4. Set hour: "\x00\x01\x53"
+5. Set minute: "\x00\x01\x54"
+
+4th byte is the actual value, 5th byte is checksum
+
+No response.
+
--- main.go
+++ main.go
@@ -17,8 +17,9 @@ var serialport string
 var emulate bool
 
 var rootCmd = &cobra.Command{
-	Use:   "nt5000-serial",
-	Short: "communicate with sunways nt5000 converter via rs232",
+	Use:     "nt5000-serial",
+	Short:   "communicate with sunways nt5000 converter via rs232",
+	Version: "1.0.0",
 }
 
 var cmdWeb = &cobra.Command{
@@ -47,6 +48,154 @@ var cmdSerial = &cobra.Command{
 		}
 		for _, port := range ports {
 			fmt.Printf("Found port: %v\n", port)
+		}
+	},
+}
+
+var settime bool
+var cmdDatetime = &cobra.Command{
+	Use:   "datetime",
+	Short: "Get or set the current time",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("args: %s\n", args)
+		log.Printf("Using serial port %s", serialport)
+
+		if settime {
+			now := time.Now().Local()
+			log.Printf("Setting date to %s\n", now.Format(time.ANSIC))
+
+			mode := &serial.Mode{
+				BaudRate: 9600,
+				Parity:   serial.NoParity,
+				DataBits: 8,
+				StopBits: serial.OneStopBit,
+			}
+
+			port, err := serial.Open("/dev/ttyUSB0", mode)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			buff := make([]byte, 5)
+			buff[0] = 0
+			buff[1] = 1
+
+			// year
+			buff[2] = 0x50
+			buff[3] = byte(now.Year() - 2000)
+			buff[4] = byte((1 + 0x50 + int(buff[3])) % 256)
+			n, err := port.Write(buff)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("Sent %v bytes\n", n)
+
+			// month
+			buff[2] = 0x51
+			buff[3] = byte(now.Month())
+			buff[4] = byte((1 + 0x51 + int(buff[3])) % 256)
+			n, err = port.Write(buff)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("Sent %v bytes\n", n)
+
+			// day
+			buff[2] = 0x52
+			buff[3] = byte(now.Day())
+			buff[4] = byte((1 + 0x52 + int(buff[3])) % 256)
+			n, err = port.Write(buff)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("Sent %v bytes\n", n)
+
+			// hour
+			buff[2] = 0x53
+			buff[3] = byte(now.Hour())
+			buff[4] = byte((1 + 0x53 + int(buff[3])) % 256)
+			n, err = port.Write(buff)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("Sent %v bytes\n", n)
+
+			// minute
+			buff[2] = 0x54
+			buff[3] = byte(now.Minute())
+			buff[4] = byte((1 + 0x54 + int(buff[3])) % 256)
+			n, err = port.Write(buff)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("Sent %v bytes\n", n)
+
+			err = port.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+		} else {
+			buff := make([]byte, 13)
+
+			log.Println("Reading current date...")
+			if emulate {
+				now := time.Now().Local()
+				fmt.Printf("now.Year(): %v\n", now.Year())
+				buff[0] = byte(now.Year() - 2000)
+				buff[1] = byte(now.Month())
+				buff[2] = byte(now.Day())
+				buff[3] = byte(now.Hour())
+				buff[4] = byte(now.Minute())
+				sum := int(0)
+				for i := 0; i < 12; i++ {
+					sum += int(buff[i])
+				}
+				buff[12] = byte(sum % 256)
+			} else {
+				mode := &serial.Mode{
+					BaudRate: 9600,
+					Parity:   serial.NoParity,
+					DataBits: 8,
+					StopBits: serial.OneStopBit,
+				}
+
+				port, err := serial.Open("/dev/ttyUSB0", mode)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				n, err := port.Write([]byte("\x00\x01\x06\x01\x08"))
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("Sent %v bytes\n", n)
+
+				port.SetReadTimeout(time.Second * 1)
+
+				for {
+					n, err := port.Read(buff)
+					if err != nil {
+						log.Fatal(err)
+						break
+					}
+					if n == 0 {
+						fmt.Println("\nEOF")
+						break
+					}
+					if n == 13 {
+						break
+					}
+				}
+
+				err = port.Close()
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+
+			t := time.Date(int(buff[0])+2000, time.Month(buff[1]), int(buff[2]), int(buff[3]), int(buff[4]), 0, 0, time.Local)
+			fmt.Printf("Current time: %s\n", t.Local().Format(time.ANSIC))
 		}
 	},
 }
@@ -190,13 +339,15 @@ func getDataPoint() DataPoint {
 }
 
 func main() {
-	fmt.Println("Hello World")
+	rootCmd.PersistentFlags().StringVarP(&serialport, "tty", "t", "/dev/ttyUSB0", "serial port")
+	rootCmd.PersistentFlags().BoolVarP(&emulate, "emulate", "e", false, "Don't use serial port, use fake data")
+
 	cmdWeb.Flags().StringVarP(&port, "port", "p", "8080", "port to listen on")
-	cmdWeb.Flags().StringVarP(&serialport, "tty", "t", "/dev/ttyUSB0", "serial port")
-	cmdWeb.Flags().BoolVarP(&emulate, "emulate", "e", false, "Don't use serial port, use fake data")
+	cmdDatetime.Flags().BoolVarP(&settime, "set", "s", false, "Sets the date and time")
 
 	rootCmd.AddCommand(cmdWeb)
 	rootCmd.AddCommand(cmdSerial)
+	rootCmd.AddCommand(cmdDatetime)
 	err := rootCmd.Execute()
 	if err != nil {
 		fmt.Println(err)
